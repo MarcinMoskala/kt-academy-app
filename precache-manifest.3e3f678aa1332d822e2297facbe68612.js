ee36d65da1099969829c30e48161ca2af0f1cd8e
Marcin Moskala <marcin.moskala@allegro.pl>
2020-08-24 11:12:39 +0200

    Updates

---
+++ precache-manifest.3e3f678aa1332d822e2297facbe68612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6c5412ac40c1962a07334ae972f000",
+    "url": "/kt-academy-app/index.html"
+  },
+  {
+    "revision": "0712e052d1c995d6df13",
+    "url": "/kt-academy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "102f8f7c804ef9ca19b2",
+    "url": "/kt-academy-app/static/js/2.aa6572e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kt-academy-app/static/js/2.aa6572e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0712e052d1c995d6df13",
+    "url": "/kt-academy-app/static/js/main.6747c117.chunk.js"
+  },
+  {
+    "revision": "bf9a2185667f9078c39b",
+    "url": "/kt-academy-app/static/js/runtime-main.813e1142.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kt-academy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MarcinMoskala/kt-academy-app/precache-manifest.cd79e5e4233b6a77cd343285b11bd40a.js"
+  "/kt-academy-app/precache-manifest.3e3f678aa1332d822e2297facbe68612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MarcinMoskala/kt-academy-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kt-academy-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
