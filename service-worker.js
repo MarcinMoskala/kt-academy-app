f58f2c89ae1a8f3b2603470c875bd6e97687ff9f
Marcin Moskala <marcin.moskala@allegro.pl>
2020-10-08 09:43:29 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://kt.academy/precache-manifest.91be1b4aff0b328cb89901094410a5f2.js"
+  "https://kt.academy/precache-manifest.8899195325729d025f68bf06c18a8a63.js"
 );
 
 self.addEventListener('message', (event) => {
